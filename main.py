8f38b0a9851c033511cb5b61d1c6691e01db0b50
ysjfanpage <ysjfanpage@gmail.com>
2025-09-13 14:01:49 +0530

    Resolved name confict

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import time
 import os
 from supabase import create_client, Client
-
+import random
 # --- Supabase setup ---
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")  
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     query = "YS Jagan"
     video_data = get_trending_video(query)
     if video_data:
-        file_name = "trending.mp4"
+        raw_file = f"trending_raw-{random.randint(1,1000)}.mp4"
         uploaded_url = upload_video_to_supabase(video_data["url"], file_name)
 
         # Caption auto-built from Twitter description + hashtags
